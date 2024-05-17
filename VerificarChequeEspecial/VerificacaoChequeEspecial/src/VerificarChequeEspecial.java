cd53c6223df637078766d2f118cc9856d66cbab0
wadzz23 <wadzz23@outlook.com>
2024-05-16 23:30:41 -0300

    +! Desafio

---
+++ VerificarChequeEspecial/VerificacaoChequeEspecial/src/VerificarChequeEspecial.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class VerificarChequeEspecial { 
+
+    public static void main(String[] args) { 
+        Scanner scanner = new Scanner(System.in); 
+
+        double saldo = scanner.nextDouble(); 
+        double saque = scanner.nextDouble(); 
+        double limiteChequeEspecial = 500; 
+
+        // TODO: Verifique se o saque não ultrapassa o saldo disponível na conta:
+         if (saque <= saldo ) {
+            System.out.println("Transacao realizada com sucesso.");
+         }
+         else if (saque > saldo && saque <= limiteChequeEspecial) {
+           System.out.println("Transacao realizada com sucesso utilizando o cheque especial.");
+           
+           }
+           else {
+          System.out.println("Transacao nao realizada. Limite do cheque especial excedido."); 
+            
+        }
+
+            // TODO: Verifique se o saque ultrapassa o limite do cheque especial, mas não o saldo total disponível:
+          
+
+                // Se o saque ultrapassar tanto o saldo disponível quanto o limite do cheque especial
+        scanner.close(); 
+
+// Fechamos o objeto Scanner para liberar os recursos:
+    }
+}
