afc9eea665ff861ace34438011a2de77d447e302
Wadson Luiz Nogueira Veloso <114113897+nogueirawadson@users.noreply.github.com>
2024-05-17 18:31:27 -0300

    Update SimulacaoBancaria.java

--- SimuladorBancario/SimulacaoBancaria/src/SimulacaoBancaria.java
+++ SimuladorBancario/SimulacaoBancaria/src/SimulacaoBancaria.java
@@ -1,7 +1,5 @@
 import java.util.Locale;
 import java.util.Scanner;
-
-
 public class SimulacaoBancaria {
     public static void main(String[] args)  {
       Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
@@ -9,21 +7,14 @@ public class SimulacaoBancaria {
       float deposito = 0;
       float saque = 0;
       int opcao = scanner.nextInt();
-     
         // Loop infinito para manter o programa em execução até que o usuário decida sair
-       
-        
          while (true) {
-        
-           
           switch (opcao) {
-           
           case 1: {
             deposito = scanner.nextFloat(); 
             saldo = deposito + saldo;
             System.out.println("Saldo Atual: " + saldo);
             break; }
-           
           case 2: {
             saque =  scanner.nextFloat();
              if (saque > saldo) {
@@ -38,12 +29,9 @@ public class SimulacaoBancaria {
           case 3: {
               System.out.println(saldo);
               break; }
-      
           case 0: {
           System.out.println("Encerrar");
             break;
-           // System.out.println("encerrar");
-            
           }
           default:
               System.out.println("Opção inválida. Tente novamente.");
